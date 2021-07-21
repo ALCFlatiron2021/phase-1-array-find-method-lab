6556d3c043bce30c2fbe47b5b21d138604947db6
Alan Cinsavich <alan.cimsavich@gmail.com>
2021-07-21 17:23:55 -0400

    commit tree 4310f85f9db837609b6e1764b35e9d82980a86cf

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record) {
+   
+
+    let yearWon = record.find((season) => season.result === "W");
+    
+    if (yearWon) { 
+        return yearWon.year
+    }
+
+}
+
+superbowlWin(record);
 
